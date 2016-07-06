a6a66f37d4fc11840409c3ff34d2fd829305a0eb
Masl <maslyay7@gmail.com>
2016-07-06 20:23:29 +0200

    Release Version 1.9.4

--- src/main/java/com/masl/mp3JUKEBOX/mp3Jukebox.java
+++ src/main/java/com/masl/mp3JUKEBOX/mp3Jukebox.java
@@ -58,7 +58,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import de.cuina.fireandfuel.CodecJLayerMP3;
 
-@Mod(modid="mp3Jukebox", name="mp3Jukebox", version="2.2.3")
+@Mod(modid="mp3Jukebox", name="mp3Jukebox", version="2.2.4")
 public class mp3Jukebox {
 
 		public static Logger logger = LogManager.getLogger("mp3Jukebox");
